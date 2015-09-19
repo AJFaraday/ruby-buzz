2f8410336363daed1c22e86eaad0a904310b3c97
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-09-19 17:58:11 +0100

    added fun lamp control to events script

--- lib/ruby_buzz.rb
+++ lib/ruby_buzz.rb
@@ -2,3 +2,5 @@ require_relative './devinput.rb'
 require_relative './button.rb'
 require_relative './light.rb'
 require_relative './pad.rb'
+
+
--- scripts/events.rb
+++ scripts/events.rb
@@ -1,16 +1,19 @@
 require_relative '../lib/ruby_buzz.rb'
 
 Pad.all.each_with_index do |pad, index|
-  pad.add_event :buzz, lambda { puts "Pushed buzzer #{index}" }
+  pad.add_event :buzz,
+                lambda {
+                  puts "Pushed buzzer #{index}"
+                  Pad.all.each{|p|p.light.off}
+                  pad.light.on
+                }
 end
 
 filename = ARGV[0] || "/dev/input/event10"
 dev = DevInput.new filename
 dev.watch
 
-n = 0
 loop do
-  n += 1
-  puts n
-  sleep 1
+  Pad.all.each{|p|p.light.off}
+  sleep 10
 end
