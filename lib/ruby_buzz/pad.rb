d845f4f8af1bde3fad0442962b713df9a9296db0
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-09-23 20:57:20 +0100

    Documented pad class

--- lib/ruby_buzz/pad.rb
+++ lib/ruby_buzz/pad.rb
@@ -1,7 +1,38 @@
-# Each of these is an individual players controller
 module RubyBuzz
+  # Each of these is an individual players controller
+  #
+  # Each USB device has four of these.
+  #
+  # Each pad has 5 buttons:
+  #
+  # * buzz
+  # * yellow
+  # * green
+  # * orange
+  # * blue
+  #
+  # Accessed by `pad.buttons[:buzz]` etc
+  #
+  # Each pad has a single LED light under the buzz button.
+  #
+  # Accessed by `pad.light`
+  #
+  # Accessing Pad objects:
+  #
+  # * `Pad.all`
+  # * `Pad[0]` etc.
+  #
+  # Defining an event on a button
+  #
+  # `pad.add_event(:buzz, lambda{ puts 'Buzz pushed!' })`
+  #
+  # Triggering a button event (for debugging)
+  #
+  # `pad.trigger_events(:buzz)`
+  #
   class Pad
 
+    # Event code mappings for each button. Split into pads.
     # {event_code => button_name}
     MAPPINGS = [
       {
@@ -36,6 +67,10 @@ module RubyBuzz
 
     attr_accessor :buttons, :light
 
+    # called by lib/ruby_buzz.rb
+    #
+    # Reads the MAPPINGS constant and creates the 4 pad objects.
+    #
     def self.init_mappings
       @@pads = []
       MAPPINGS.each_with_index do |mapping, index|
@@ -43,14 +78,28 @@ module RubyBuzz
       end
     end
 
+    #
+    # Returns all four pads in index order.
+    #
     def self.all
       @@pads
     end
 
+    #
+    # Call a specific pad by index (0 to 3)
+    #
     def self.[](index)
       @@pads[index]
     end
 
+    #
+    # Initialize pad objects, called by init_mappings
+    #
+    # Arguments:
+    #
+    # * mapping - hash of event codes against button name (from MAPPINGS)
+    # * index - index of this pad, from 0 to 3
+    #
     def initialize(mapping, index)
       @index = index
       @buttons = {}
@@ -60,10 +109,31 @@ module RubyBuzz
       end
     end
 
+    #
+    # Add an event mapping, to be triggered on button push.
+    #
+    # Arguments:
+    #
+    # * button_name - Symbol, name of the button to be pushed
+    # * proc - Proc, a ruby method to be called.
+    #
+    # As the Proc is run on a separate thread, with the same environment,
+    # it is wise to change data in a shared space (for instance, a class
+    # variable or a setter method) for the main body of your code to
+    # respond to instead of doing the heavy lifting in the button event.
+    #
     def add_event(button_name, proc)
       @buttons[button_name].add_event proc
     end
 
+    #
+    # For debugging: Trigger a button event directly.
+    # Also called by watcher in RubyBuzz::Device.
+    #
+    # Arguments:
+    #
+    # * button_name - Symbol name of buzzer (:buzz, :yellow, :green, :orange, :blue)
+    #
     def trigger_event(button_name)
       @buttons[button_name].trigger_events
     end
