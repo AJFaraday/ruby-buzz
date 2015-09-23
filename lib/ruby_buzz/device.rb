7dc63926f66f9424368b53f41a2f9c79071fc579
ajfaraday <andrewfaraday@hotmail.co.uk>
2015-09-23 21:42:05 +0100

    Documented the Device class

--- lib/ruby_buzz/device.rb
+++ lib/ruby_buzz/device.rb
@@ -1,6 +1,19 @@
-#!/usr/bin/env ruby
-
 module RubyBuzz
+  #
+  # The main interface for the Buzz controllers. Primarily
+  # used to monitor key pushes and trigger events. Keep a single
+  # instance of the class:
+  #
+  # `RubyBuzz::Device.new`
+  #
+  # The `each` method exposes events directly as they come in
+  #
+  # `device.each { |event| puts event }`
+  #
+  # The `start_watching` method starts a background job which
+  # runs the events bound to each button via the RubyBuzz::Button
+  # class. You can end this worker with `stop_watching`.
+  #
   class Device
 
     require 'time'
@@ -12,6 +25,8 @@ module RubyBuzz
 
     Event = Struct.new(:tv_sec, :tv_usec, :type, :code, :value)
     # open Event class and add some convenience methods
+    #
+    # Holds the un-packed data parsed from the raw input from the buzz controller.
     class Event
       def time;
         Time.at(tv_sec)
@@ -25,6 +40,12 @@ module RubyBuzz
       end
     end
 
+    #
+    # Initialise device, getting event file from /dev/input/by-id/
+    #
+    # You can override this to a different event, but ruby-buzz only
+    # supports a single usb controller.
+    #
     def initialize(filename=nil)
       @dev = File.open(filename || DEFAULT_FILE_NAME)
       @block_size = 24
@@ -33,16 +54,24 @@ module RubyBuzz
       raise er
     end
 
+    #
+    # The format for each 24 bit data chunk taken from the event stream.
+    #
     def format
       'qqSSl'
     end
 
-
+    #
+    # Read a single 24-bit block.
+    #
     def read
       bin = @dev.read @block_size
       Event.new *bin.unpack(format)
     end
 
+    #
+    # Expose each event to a block of code as it comes in.
+    #
     def each
       begin
         loop do
@@ -55,7 +84,10 @@ module RubyBuzz
       end
     end
 
-
+    #
+    # Start a background worker which scans input file
+    # and triggers any events bound to each one.
+    #
     def start_watching
       return if @worker
       @worker = Thread.new do
@@ -68,6 +100,9 @@ module RubyBuzz
       end
     end
 
+    #
+    # Stop the background worker, release it's resources.
+    #
     def stop_watching
       @worker.kill
       @worker = nil
